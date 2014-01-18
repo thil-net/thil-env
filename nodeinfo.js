dfb2e62faa8ed1b3794beab789d3151e20d069ac
David Pitman <me@davidpitman.name>
2014-01-18 16:23:51 -0700

    Create nodeinfo.js

---
+++ nodeinfo.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+var os = require('os');
+console.log({
+  "process": {
+    "argv": process.argv, 
+    "execPath": process.execPath, 
+    "execArgv": process.execArgv, 
+    "cwd": process.cwd(), 
+    "env": process.env, 
+    "gid": process.getgid(), 
+    "uid": process.getuid(), 
+    "groups": process.getGroups(), 
+    "version": process.version, 
+    "versions": process.versions, 
+    "config": process.config, 
+    "arch": process.arch, 
+    "platform": process.platform
+  }, 
+  "os": {
+    "endianness": os.endianness(), 
+    "hostname": os.hostname(), 
+    "type": os.type(), 
+    "platform": os.platform(), 
+    "arch": os.arch(), 
+    "release": os.release(), 
+    "uptime": os.uptime(), 
+    "loadavg": os.loadavg(), 
+    "totalmem": os.totalmem(), 
+    "freemem": os.freemem(), 
+    "cpus": os.cpus(), 
+    "networkInterfaces": os.networkInterfaces(), 
+    "EOL": os.EOL
+  }
+});
